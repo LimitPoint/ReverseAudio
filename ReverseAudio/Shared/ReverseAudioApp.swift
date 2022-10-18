b83bdfa4744238bc68a4e0df2faa3ca2554f31b5
“jpagliaro” <jpagliaro@me.com>
2022-10-18 19:13:36 -0400

    Add links
    Added relevant links to our website `Reversing Audio Files` blog discussion, in file header sections

--- ReverseAudio/Shared/ReverseAudioApp.swift
+++ ReverseAudio/Shared/ReverseAudioApp.swift
@@ -2,6 +2,9 @@
 //  ReverseAudioApp.swift
 //  ReverseAudio
 //
+//  Read discussion at:
+//  http://www.limit-point.com/blog/2022/reverse-audio/
+//
 //  Created by Joseph Pagliaro on 1/16/22.
 //  Copyright © 2022 Limit Point LLC. All rights reserved.
 //
